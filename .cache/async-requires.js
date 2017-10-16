e6ad42498bf60969375dbccb1568da3bc0407519
Yasser Zubair <Yasserzubair68@gmail.com>
2017-10-16 14:05:28 +0500

    Deployed

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,19 +2,16 @@
 const preferDefault = m => m && m.default || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": require("gatsby-module-loader?name=component---cache-dev-404-page-js!/Users/komalpervez/projects/NooBs2/.cache/dev-404-page.js"),
-  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/komalpervez/projects/NooBs2/src/pages/index.js"),
   "component---src-pages-404-js": require("gatsby-module-loader?name=component---src-pages-404-js!/Users/komalpervez/projects/NooBs2/src/pages/404.js"),
+  "component---src-pages-index-js": require("gatsby-module-loader?name=component---src-pages-index-js!/Users/komalpervez/projects/NooBs2/src/pages/index.js"),
   "component---src-pages-page-2-js": require("gatsby-module-loader?name=component---src-pages-page-2-js!/Users/komalpervez/projects/NooBs2/src/pages/page-2.js")
 }
 
 exports.json = {
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("gatsby-module-loader?name=path---dev-404-page!/Users/komalpervez/projects/NooBs2/.cache/json/dev-404-page.json"),
+  "404.json": require("gatsby-module-loader?name=path---404!/Users/komalpervez/projects/NooBs2/.cache/json/404.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
   "index.json": require("gatsby-module-loader?name=path---index!/Users/komalpervez/projects/NooBs2/.cache/json/index.json"),
-  "layout-index.json": require("gatsby-module-loader?name=path---!/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
-  "404.json": require("gatsby-module-loader?name=path---404!/Users/komalpervez/projects/NooBs2/.cache/json/404.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
   "page-2.json": require("gatsby-module-loader?name=path---page-2!/Users/komalpervez/projects/NooBs2/.cache/json/page-2.json"),
   "layout-index.json": require("gatsby-module-loader?name=path---!/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -3,19 +3,16 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/komalpervez/projects/NooBs2/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/komalpervez/projects/NooBs2/src/pages/index.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/komalpervez/projects/NooBs2/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/komalpervez/projects/NooBs2/src/pages/index.js")),
   "component---src-pages-page-2-js": preferDefault(require("/Users/komalpervez/projects/NooBs2/src/pages/page-2.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
-  "dev-404-page.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/dev-404-page.json"),
+  "404.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/404.json"),
   "layout-index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
   "index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/index.json"),
-  "layout-index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
-  "404.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/404.json"),
   "layout-index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
   "page-2.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/page-2.json"),
   "layout-index.json": require("/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"),
