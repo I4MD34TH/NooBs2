e68dac0801cfc71c8e14a2171b8f4c059a59ba22
Yasser Zubair <Yasserzubair68@gmail.com>
2017-10-16 13:46:14 +0500

    Changes in mobile view

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/komalpervez/projects/NooBs2/src/layouts/index.js"
+  import data from "/Users/komalpervez/projects/NooBs2/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
